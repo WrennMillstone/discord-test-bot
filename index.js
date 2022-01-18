5364fa9f39bd2c8b07230f43d7fb40cc6ec6a87d
Michael St. Clare <85242153+WrennMillstone@users.noreply.github.com>
2022-01-18 13:01:25 -0500

    add command handling for sample commands

--- index.js
+++ index.js
@@ -10,5 +10,20 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+//listen for commands and respon appropriatly
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
