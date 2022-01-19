c322e6013538d7b76d020492bcde2f07cb51af2e
Michael St. Clare <wydrea@gmail.com>
2022-01-18 19:11:19 -0500

    set up dynamic command importing

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-//listen for commands and respon appropriatly
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
@@ -18,12 +25,9 @@ client.on('interactionCreate', async interaction => {
 
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
 	}
 });
 
-// Login to Discord with your client's token
 client.login(token);
