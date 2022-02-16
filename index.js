82cf62c93b1d64e6e3ab5247ea02e2b59ff87f7e
Michael St. Clare <85242153+WrennMillstone@users.noreply.github.com>
2022-02-16 12:14:22 -0500

    update intents

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({ intents: [
+	Intents.FLAGS.GUILDS, 
+	Intents.FLAGS.GUILD_VOICE_STATES,
+	Intents.FLAGS.GUILD_MESSAGES,
+	Intents.FLAGS.GUILD_MEMBERS,
+	Intents.FLAGS.GUILD_PRESENCES,
+] });
 
 client.commands = new Collection();
 
